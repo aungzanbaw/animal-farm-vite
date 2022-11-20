c35a05321f7d5479326fa0d00e9c95b0f226b0ed
Aung Zan Baw <aungzanbaw@gmail.com>
2022-11-20 15:28:18 -0800

    useState, useEff, custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery')
+    search(lastQuery)
+  },[])
+
+  async function search(q) {
+    const response = await fetch("http://localhost:8080?" + new URLSearchParams({q}))
+    const data = await response.json()
+    setAnimals(data)
+    localStorage.setItem('lastQuery', q)
+  }
+
+  return {search, animals}
+}
+
+function App() { 
+  const {search, animals} = useAnimalSearch()
 
   return (
-    <div className="App">
-      <img src="https://yt3.ggpht.com/ytc/AMLnZu80d66aj0mK3KEyMfpdGFyrVWdV5tfezE17IwRkhw=s88-c-k-c0x00ffffff-no-rj" />
-    </div>
+    <main className="App">
+      <h1>Animal Farm</h1>
+      <input type="text" placeholder='Search Animals' onChange={(e) => search(e.target.value)}/>
+
+      <ul>
+        { animals.map(animal => (
+          <Animals key={animal.id} {...animal}/>
+        ))}
+
+        {animals.length == 0 && 'No Animals found'}
+      </ul>
+
+    </main>
+  )
+}
+
+function Animals({name, age, type}){
+  return(
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
   )
 }
 
